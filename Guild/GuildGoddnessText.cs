872cd526d3e2a3e29fba0dcfae62b749c6a973fc
sakura68 <62635306+sakura68@users.noreply.github.com>
2020-03-25 19:55:19 +0900

    Create GuildGoddnessText.cs

---
+++ Guild/GuildGoddnessText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuildGoddnessText : MonoBehaviour
+{
+    public enum enGuildGoddnessText_Type
+    {
+        GuildExp = 1,
+        GuildContribution,      // 기여도
+        BuffGold,
+        //BuffUserExpPercent,
+        BuffCreatureExpPercent,
+        //BuffDuration,
+        BuffRewardKey,
+    }
+
+    //===================================================================================
+    //
+    // Field
+    //
+    //===================================================================================
+    [SerializeField] private UILabel _Label;
+
+    //===================================================================================
+    //
+    // Variable
+    //
+    //===================================================================================
+    private enGuildGoddnessText_Type _TextType;
+    public enGuildGoddnessText_Type TextType { get { return _TextType; } }
+
+    //===================================================================================
+    //
+    // Method
+    //
+    //===================================================================================
+    public void SetText(string text, enGuildGoddnessText_Type type)
+    {
+        _Label.text = text;
+        _TextType = type;
+    }
+}
