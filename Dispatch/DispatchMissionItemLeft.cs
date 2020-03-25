9087cf89202ca5a39d6dbd296e495cfe34f9627c
sakura68 <62635306+sakura68@users.noreply.github.com>
2020-03-25 19:54:33 +0900

    Create DispatchMissionItemLeft.cs

---
+++ Dispatch/DispatchMissionItemLeft.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DGL_DATA_READER;
+
+public class DispatchMissionItemLeft : MonoBehaviour
+{
+    //===================================================================================
+    //
+    // Field
+    //
+    //===================================================================================
+    [SerializeField] private UILabel m_TitleLabel;
+    [SerializeField] private UI2DSprite m_RegionSprite;
+    [SerializeField] private UISprite m_SelectSprite;
+
+    //===================================================================================
+    //
+    // Variable
+    //
+    //==================================================================================
+    private DATA_DISPATCH m_DispatchTableData = null;
+    public DATA_DISPATCH DispatchTableData { get { return m_DispatchTableData; } }
+
+    //===================================================================================
+    //
+    // Method
+    //
+    //===================================================================================
+    public void Init(DATA_DISPATCH DispatchTableData)
+    {
+        m_DispatchTableData = DispatchTableData;
+
+        DATA_DISPATCH_CATEGORY DispatchCategoryData = CDATA_DISPATCH_CATEGORY.Get(m_DispatchTableData.DispatchCategory);
+        if(DispatchCategoryData != null)
+        {
+            m_TitleLabel.text = StringTableManager.GetData(DispatchCategoryData.String_Name);
+            m_RegionSprite.sprite2D = UIResourceMgr.CreateSprite(BUNDLELIST.TEXTURE_ICON_DISPATCH, DispatchCategoryData.Image_Name);
+        }
+
+        SetActiveSelect(false);
+    }
+
+    public void SetActiveSelect(bool bIsActive)
+    {
+        m_SelectSprite.gameObject.SetActive(bIsActive);
+    }
+}
